e790eb122dd1e3b25f54e19c704189d298bf5be4
tr0ythex <tr0ythex@gmail.com>
2016-12-04 00:12:00 +0300

    Создана модель post для хранения последних данных по запросу. Создана функция перерисовки DOM в случае изменения размеров окна. Переименованы некоторые классы и переменные. Добавлен мета тэг viewport

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const globalSettings = {
     'baseUrl': 'https://api.instagram.com/v1',
     'accessToken': '691623.1419b97.479e4603aff24de596b1bf18891729f3',
-    'callback': 'showRecentMedia',
+    'callback': 'saveRecentMedia',
     'queryParams': {
         'count': 20
-    }
+    },
+    colCount: 3,
+    widthLarge: 920,
+    widthMedium: 580
 };
+
+const recentPosts = [];
 
 class User {
     constructor(userId) {
@@ -30,81 +35,111 @@ class User {
     }
 }
 
-function showRecentMedia(resp) {
-    for (let i = 0; i < resp.data.length; i++) {
-        let mediaItemDiv = document.createElement('div');
-        mediaItemDiv.className = 'media-item';
+class Post {
+    constructor(
+        id,
+        avatar,
+        author,
+        location,
+        timeAgo,
+        picture,
+        likesCount,
+        caption
+    ) {
+        this.id = id;
+        this.avatar = avatar;
+        this.author = author;
+        this.location = location;
+        this.timeAgo = timeAgo;
+        this.picture = picture;
+        this.likesCount = likesCount;
+        this.caption = caption;;
+    }
+}
 
-        let mediaItemHeader = document.createElement('div')
-        let mediaItemPicture = document.createElement('div')
-        let mediaItemLikes = document.createElement('div')
-        let mediaItemDescription = document.createElement('div')
+function saveRecentMedia(resp) {
+    for (let post of resp.data) {
+        recentPosts.push(
+            new Post(
+                post.id,
+                post.user.profile_picture,
+                post.user.username,
+                post.location ? post.location.name : '',
+                getTimeDiff(new Date(), new Date(post.created_time * 1000)),
+                post.images.standard_resolution.url,
+                post.likes.count,
+                post.caption.text
+            )
+        );
+    }
+    displayRecentMedia();
+}
 
-        mediaItemHeader.className = 'media-item__header';
-        mediaItemPicture.className = 'media-item__picture';
-        mediaItemLikes.className = 'media-item__likes';
-        mediaItemDescription.className = 'media-item__description';
+function displayRecentMedia() {
+    recentPosts.forEach((post, index) => {
+        const postCard            = document.createElement('div');
+        const postCardHeader      = document.createElement('div');
+        const postCardPicture     = document.createElement('div');
+        const postCardLikes       = document.createElement('div');
+        const postCardDescription = document.createElement('div');
+        const prefix = 'post-card';
 
-        // profile img
-        let profileImg = document.createElement('img');
-        profileImg.src = resp.data[i].user.profile_picture;
-        profileImg.classList.add('img-round', 'img-profile');
+        postCard.className            = `${prefix}`;
+        postCardHeader.className      = `${prefix}__header`;
+        postCardPicture.className     = `${prefix}__picture`;
+        postCardLikes.className       = `${prefix}__likes`;
+        postCardDescription.className = `${prefix}__description`;
 
-        // username and location
-        let usernameLocationDiv = document.createElement('div');
-        usernameLocationDiv.className = 'username-location';
-        let usernameDiv = document.createElement('div');
-        usernameDiv.className = 'username';
-        usernameDiv.innerText = resp.data[i].user.username;
-        let locationDiv = document.createElement('div');
-        locationDiv.className = 'location';
-        locationDiv.innerText = resp.data[i].location ? resp.data[i].location.name : '';
-        usernameLocationDiv.appendChild(usernameDiv);
-        usernameLocationDiv.appendChild(locationDiv);
+        const avatar           = document.createElement('img');
+        const usernameLocation = document.createElement('div');
+        const username         = document.createElement('div');
+        const location         = document.createElement('div');
+        const timeAgo          = document.createElement('div');
 
-        // time ago
-        let timeAgoDiv = document.createElement('div');
-        timeAgoDiv.className = 'time-ago';
-        let createdDate = new Date(resp.data[i].created_time * 1000);
-        let currentDate = new Date();
-        timeAgoDiv.innerText = this.getTimeDiff(currentDate, createdDate);
+        avatar.classList.add('img-round', 'img-profile');
+        usernameLocation.className = 'username-location';
+        username.className = 'username';
+        location.className = 'location';
+        timeAgo.className = 'time-ago';
 
-        // header
-        mediaItemHeader.appendChild(profileImg);
-        mediaItemHeader.appendChild(usernameLocationDiv);
-        mediaItemHeader.appendChild(timeAgoDiv);
+        avatar.src = post.avatar;
+        username.innerText = post.author;
+        location.innerText = post.location;
+        timeAgo.innerText = post.timeAgo;
 
-        // post pic
-        let postImg = document.createElement('img');
-        postImg.src = resp.data[i].images.standard_resolution.url;
-        postImg.className = 'img-post';
-        mediaItemPicture.appendChild(postImg);
+        usernameLocation.appendChild(username);
+        usernameLocation.appendChild(location);
+        postCardHeader.appendChild(avatar);
+        postCardHeader.appendChild(usernameLocation);
+        postCardHeader.appendChild(timeAgo);
 
-        // likes
-        let heart = document.createElement('span');
+        const picture = document.createElement('img');
+        picture.className = 'img-post';
+        picture.src = post.picture;
+        postCardPicture.appendChild(picture);
+
+        const heart = document.createElement('span');
+        const likesCount = document.createElement('span');
+        heart.className = 'heart';
         heart.innerHTML = '&#9825; ';
         heart.addEventListener('click', () => {
-            alert(`Id записи: ${resp.data[i].id}`);
+            alert(`Id записи: ${post.id}`);
         });
-        heart.className = 'heart';
-        let likesCount = document.createElement('span');
-        likesCount.innerText = resp.data[i].likes.count
-        mediaItemLikes.appendChild(heart);
-        mediaItemLikes.appendChild(likesCount);
+        likesCount.innerText = post.likesCount;
+        postCardLikes.appendChild(heart);
+        postCardLikes.appendChild(likesCount);
 
-        // description
-        mediaItemDescription.innerText = resp.data[i].caption.text;
+        postCardDescription.innerText = post.caption;
 
-        // media final div
-        mediaItemDiv.appendChild(mediaItemHeader);
-        mediaItemDiv.appendChild(mediaItemPicture);
-        mediaItemDiv.appendChild(mediaItemLikes);
-        mediaItemDiv.appendChild(mediaItemDescription);
+        postCard.appendChild(postCardHeader);
+        postCard.appendChild(postCardPicture);
+        postCard.appendChild(postCardLikes);
+        postCard.appendChild(postCardDescription);
 
-        // column
-        let colElem = document.getElementsByClassName('col')[i % 3];
-        colElem.appendChild(mediaItemDiv);
-    }
+        const colElems = document.getElementsByClassName('col');
+        const colElem = colElems[index % colElems.length];
+        colElem.appendChild(postCard);
+    });
 }
 
 function getTimeDiff(currentDate, createdDate) {
@@ -128,10 +163,54 @@ function addScript(src) {
     document.body.appendChild(scriptElement);
 }
 
+function createBasicStructure(colCount) {
+    while (document.querySelector('.col')) {
+        document.querySelector('.col').remove();
+    }
+
+    for (let i = 0; i < colCount; i++) {
+        let col = document.createElement('div');
+        let script = document.querySelector('script');
+        col.className = 'col';
+        document.body.insertBefore(col, script);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    if (window.innerWidth < globalSettings.widthMedium) {
+        globalSettings.colCount = 1;
+    } else if (window.innerWidth < globalSettings.widthLarge) {
+        globalSettings.colCount = 2;
+    } else {
+        globalSettings.colCount = 3;
+    }
+    createBasicStructure(globalSettings.colCount);
+
     const user = new User(691623);
     const src = user.getRecentMediaUrl(globalSettings);
     if (src) {
         addScript(src);
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < globalSettings.widthMedium) {
+        if (globalSettings.colCount !== 1) {
+            globalSettings.colCount = 1;
+            createBasicStructure(globalSettings.colCount);
+            displayRecentMedia();
+        }
+    } else if (window.innerWidth < globalSettings.widthLarge) {
+        if (globalSettings.colCount !== 2) {
+            globalSettings.colCount = 2;
+            createBasicStructure(globalSettings.colCount);
+            displayRecentMedia();
+        }
+    } else {
+        if (globalSettings.colCount !== 3) {
+            globalSettings.colCount = 3;
+            createBasicStructure(globalSettings.colCount);
+            displayRecentMedia();
+        }
     }
 });
